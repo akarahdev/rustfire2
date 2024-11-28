c2fcd1e9ac2dc47732758e3c944ccd27f1fbc763
akarahdev <endisticbutitsforgit@gmail.com>
2024-11-27 19:01:51 -0500

    feat: add utility macros for simple values

--- dfexe/src/main.rs
+++ dfexe/src/main.rs
@@ -3,13 +3,14 @@ use rustfire::api::event::PlayerEvent;
 use rustfire::api::items::component::Component;
 use rustfire::api::items::number::Number;
 use rustfire::api::player::Player;
+use rustfire::{comp, num};
 
 fn main() {
     PlayerEvent::join(|| {
-        Player::send_message(Component::new("Hello world!"));
+        Player::send_message(comp!("Hello world!"));
         Player::send_message(
-                Component::new("Ok! The solution is: ") +
-                    (Number::new("1.23") / Number::new("2.53"))
+                comp!("Ok! The solution is: ") +
+                    (num!("1.23") / num!("2.53"))
         );
     });
     done();
--- dflib/src/api/items/mod.rs
+++ dflib/src/api/items/mod.rs
@@ -9,3 +9,18 @@ use crate::codetemplate::args::Item;
 pub trait VarItem: Clone {
     fn as_item(&self) -> Item;
 }
+
+#[macro_export]
+macro_rules! num {
+    ($t:expr) => { $crate::api::items::number::Number::new($t) }
+}
+
+#[macro_export]
+macro_rules! str {
+    ($t:expr) => { $crate::api::items::string::String::new($t) }
+}
+
+#[macro_export]
+macro_rules! comp {
+    ($t:expr) => { $crate::api::items::component::Component::new($t) }
+}
