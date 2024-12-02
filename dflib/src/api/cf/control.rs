dc416ac8ee51d85b5769917b64c9376867043770
akarahdev <endisticbutitsforgit@gmail.com>
2024-12-01 20:13:48 -0500

    feat: some control flow actions

--- dflib/src/api/cf/control.rs
+++ dflib/src/api/cf/control.rs
@@ -7,14 +7,40 @@ pub struct Control;
 
 impl Control {
     pub fn wait(dur: Duration) {
-        push_block(
-            TemplateBlock::control(
-                "Wait",
-                ChestArgs::new()
-                    .with_slot(0, Item::Number { data: NamedData { name: dur.0.to_string() } })
-                    .with_slot(26, Item::block_tag("Ticks", "Time Unit",
-                                                   "Wait", BlockType::Control)),
-            )
-        );
+        push_block(TemplateBlock::control(
+            "Wait",
+            ChestArgs::new()
+                .with_slot(0, Item::Number { data: NamedData { name: dur.0.to_string() } })
+                .with_slot(26, Item::block_tag("Ticks", "Time Unit",
+                                               "Wait", BlockType::Control)),
+        ));
+    }
+
+    pub fn return_from_function() {
+        push_block(TemplateBlock::control(
+            "Return",
+            ChestArgs::new(),
+        ));
+    }
+
+    pub fn end_thread() {
+        push_block(TemplateBlock::control(
+            "End",
+            ChestArgs::new(),
+        ));
+    }
+
+    pub fn skip_iteration() {
+        push_block(TemplateBlock::control(
+            "Skip",
+            ChestArgs::new(),
+        ));
+    }
+
+    pub fn stop_repeat() {
+        push_block(TemplateBlock::control(
+            "StopRepeat",
+            ChestArgs::new(),
+        ));
     }
 }
--- dflib/src/api/cf/repeat.rs
+++ dflib/src/api/cf/repeat.rs
@@ -1,8 +1,11 @@
 use crate::api::items::number::Number;
 use crate::api::{allocate_variable, push_block};
+use crate::api::items::dict::Dictionary;
+use crate::api::items::list::List;
+use crate::api::items::loc::Location;
 use crate::api::items::VarItem;
-use crate::codetemplate::args::ChestArgs;
-use crate::codetemplate::template::{BracketDirection, BracketType, TemplateBlock};
+use crate::codetemplate::args::{ChestArgs, Item};
+use crate::codetemplate::template::{BlockType, BracketDirection, BracketType, TemplateBlock};
 
 pub struct Repeat;
 
@@ -24,6 +27,67 @@ impl Repeat {
         ));
         push_block(TemplateBlock::bracket(BracketDirection::Start, BracketType::Sticky));
         code(Number(idx.clone()));
+        push_block(TemplateBlock::bracket(BracketDirection::End, BracketType::Sticky));
+    }
+
+    pub fn range<F: FnOnce(Number)>(start: Number, end: Number, step: Number, code: F) {
+        let idx = allocate_variable();
+        push_block(TemplateBlock::repeat(
+            "Multiple",
+            ChestArgs::new()
+                .with_slot(0, idx.clone())
+                .with_slot(1, start.as_item())
+                .with_slot(2, end.as_item())
+                .with_slot(3, step.as_item()),
+        ));
+        push_block(TemplateBlock::bracket(BracketDirection::Start, BracketType::Sticky));
+        code(Number(idx.clone()));
+        push_block(TemplateBlock::bracket(BracketDirection::End, BracketType::Sticky));
+    }
+
+    pub fn for_each<T: VarItem, F: FnOnce(T)>(list: List<T>, code: F) {
+        let idx = allocate_variable();
+        push_block(TemplateBlock::repeat(
+            "ForEach",
+            ChestArgs::new()
+                .with_slot(0, idx.clone())
+                .with_slot(1, list.as_item())
+                .with_slot(26, Item::block_tag("True", "Allow List Changes",
+                                               "ForEach", BlockType::Repeat)),
+        ));
+        push_block(TemplateBlock::bracket(BracketDirection::Start, BracketType::Sticky));
+        code(T::from_item(idx.clone()));
+        push_block(TemplateBlock::bracket(BracketDirection::End, BracketType::Sticky));
+    }
+
+    pub fn for_each_entry<K: VarItem, V: VarItem, F: FnOnce(K, V)>(dict: Dictionary<K, V>, code: F) {
+        let k = allocate_variable();
+        let v = allocate_variable();
+        push_block(TemplateBlock::repeat(
+            "ForEachEntry",
+            ChestArgs::new()
+                .with_slot(0, k.clone())
+                .with_slot(1, v.clone())
+                .with_slot(2, dict.as_item())
+                .with_slot(26, Item::block_tag("True", "Allow List Changes",
+                                               "ForEach", BlockType::Repeat)),
+        ));
+        push_block(TemplateBlock::bracket(BracketDirection::Start, BracketType::Sticky));
+        code(K::from_item(k), V::from_item(v));
+        push_block(TemplateBlock::bracket(BracketDirection::End, BracketType::Sticky));
+    }
+
+    pub fn range<F: FnOnce(Location)>(start: Location, end: Location, code: F) {
+        let idx = allocate_variable();
+        push_block(TemplateBlock::repeat(
+            "Grid",
+            ChestArgs::new()
+                .with_slot(0, idx.clone())
+                .with_slot(1, start.as_item())
+                .with_slot(2, end.as_item())
+        ));
+        push_block(TemplateBlock::bracket(BracketDirection::Start, BracketType::Sticky));
+        code(Location(idx.clone()));
         push_block(TemplateBlock::bracket(BracketDirection::End, BracketType::Sticky));
     }
 }
